c3b7736965427e6838ba9e5aea0468165f555f75
ArtMoc <tem.mochalov@yandex.ru>
2021-11-16 21:54:57 +0300

    Add project files.

---
+++ Academy/academy.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+	string last_name;
+	string first_name;
+	unsigned int age;
+public:
+	const string& get_last_name()const
+	{
+		return last_name;
+	}
+	const string& get_first_name()const
+	{
+		return first_name;
+	}
+	unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+	//                 CONSTRUCTORS:
+	Human(const string& last_name, const string& first_name, unsigned int age)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "H_Constructor:\t" << this << endl;
+	}
+	~Human()
+	{
+		cout << "H_Destructor:\t" << this << endl;
+	}
+	//                METHODS:
+	void print()const
+	{
+		cout << last_name << " " << first_name << " " << age << " лет.\n";
+	}
+};
+
+class Student:public Human
+{
+	string speciality;
+	string group;
+	double rating;
+public:
+	const string& get_speciality()const
+	{
+		return speciality;
+	}
+	const string& get_group()const
+	{
+		return group;
+	}
+	double get_rating()const
+	{
+		return rating;
+	}
+	void set_speciality(const string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+	//                       COUNSTRUCTOR:
+	Student
+	(
+		const string& last_name, const string& first_name, unsigned int age,
+		const string& speciality, const string& group, double rating
+	):Human(last_name, first_name, age)//Делегируем конструктор базового класса
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		cout << "S_Constructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "S_Destructor:\t" << this << endl;
+	}
+	//                 METHODS:
+	void print()const
+	{
+		Human::print();
+		cout << "Специальность: " << speciality 
+			<< ", группа: " << group
+			<< "успеваемость: " << rating << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Human h("Montana", "Antonio", 35);
+	h.print();
+
+	Student s("Pinkman", "Jessie", 22, "Methamphitamine manufacturing", "WW_01 ", 93);
+	s.print();
+}
