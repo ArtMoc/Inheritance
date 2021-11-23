7c86e2331f87fab9c2066d5b55835b4eadf392a7
ArtMoc <tem.mochalov@yandex.ru>
2021-11-23 16:04:42 +0300

    Files ClassWork

---
+++ Files/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//#define WRITE_TO_FILE
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef WRITE_TO_FILE
+	//cout << "Hello World" << endl;
+	ofstream fout("File.txt", std::ios_base::app); //Создаем и открываем поток
+	fout << "Hello World" << endl;
+	fout.close(); //В конце поток открытия файлов обязательно нужно закрыть
+	system("notepad File.txt");
+#endif // WRITE_TO_FILE
+
+	const int SIZE = 256;
+	char buffer[SIZE] = {};
+	ifstream fin("File.txt");
+	if (fin.is_open())
+	{
+		while (!fin.eof()) //Пока не end of file
+		{
+			//fin >> buffer;
+			fin.getline(buffer, SIZE);
+			cout << buffer << endl;
+		}
+	}
+	else
+	{
+		cerr << "File not founf" << endl;
+	}
+	fin.close();
+}
