290efa6ca4a38b35a7b6b6f0d90007d97da31920
ArtMoc <tem.mochalov@yandex.ru>
2021-11-23 12:55:40 +0300

    1

--- Academy/academy.cpp
+++ Academy/academy.cpp
@@ -186,9 +186,12 @@ public:
 	}
 };
 
+//#define INHERITANCE
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef INHERITANCE
 	Human h("Montana", "Antonio", 35);
 	h.print();
 
@@ -202,4 +205,25 @@ void main()
 		"Cryminalistic", "OBN", 95,
 		"How to catch Heisenberg");
 	g.print();
+#endif // INHERITANCE
+
+
+	//Generalisation:
+	Human* group[] =
+	{
+		new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_01", 93),//upcast
+		new Student("Vercetti", "Tomas", 30, "Cryminal", "Vice", 90),//upcast
+		new Teacher("White", "Walter", 50, "Chemistry", 25),//upcast
+		new Student("Diaz", "Ricardo", 55, "Weapons distribution", "Vice", 80),
+		new Graduate("Schrader", "Hank", 42,
+		"Cryminalistic", "OBN", 95,	"How to catch Heisenberg"),
+		new Teacher("Eistein", "Albert", 143, "Astronomy", 120)
+	};
+	//Specialisatin
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		cout << "\n----------------------------------------\n";
+		group[i]->print();
+	}
+	cout << "\n----------------------------------------\n";
 }
